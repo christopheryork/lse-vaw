b7e8597ce0dcd103831b124080c927b17ae8e5ad
Christopher W York <christopher.w.york@gmail.com>
2016-04-15 10:47:04 +0100

    Move back to ES5 for Safari

--- timeline.js
+++ timeline.js
@@ -1,12 +1,12 @@
 function timeline(elem, data, callback) {
 
-    let nest = d3.nest()
+    var nest = d3.nest()
       .key(function(d) { return d.period })
       .sortKeys(function(a,b) { return d3.ascending(period(a)[0], period(b)[0]) })
       .sortValues(function(a,b) { return d3.ascending(a.year, b.year) })
       .entries(data)
 
-    let section = d3.select(elem).selectAll('section')
+    var section = d3.select(elem).selectAll('section')
         .data(nest)
       .enter().append('section')
         .attr('class', 'period')
@@ -14,7 +14,7 @@ function timeline(elem, data, callback) {
     section.append('h2')
         .html(function(d) { return d.key })
 
-    let entry = section.append('ul').selectAll('li')
+    var entry = section.append('ul').selectAll('li')
           .data( function(d) { return d.values })
         .enter().append('li')
 
